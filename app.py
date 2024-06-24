81dc03efff4c6f563ce6e18536dd92b663e013e2
quan08 <quan08012008@gmail.com>
2024-06-24 22:25:30 +0700

    n

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import customtkinter as ctk
 
+
 class DrawingTool(ctk.CTk):
+    ver = "1.0.0"
+    font1 = ctk.CTkFont(family="JetBrains Mono", size=13)
+
     def __init__(self):
         super().__init__()
         self.title("Drawing Tool 1.0")
         self.geometry("960x640")
-        self._set_appearance_mode("light")     
-    
+        text1 = ctk.CTkLabel(
+            self, font=self.font1, text=f"CurrentVer: {self.ver}", text_color="white"
+        )
+        text1.pack(anchor="sw", padx=10)
+
 
 if __name__ == "__main__":
     app = DrawingTool()
